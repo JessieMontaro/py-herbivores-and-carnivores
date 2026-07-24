f1c2b15d7d50430b4ff051245793bfac248f7d44
Jessie Montaro <montaro.daedra@gmail.com>
2026-07-24 12:38:09 +0200

    Moved the alive check to the animal class

--- app/main.py
+++ app/main.py
@@ -4,9 +4,22 @@ class Animal:
 
     def __init__(self, name: str, *args) -> None:
         self.name = name
-        self.health = 100
+        if args != ():
+            self.health = args[0]
+        else:
+            self.health = 100
         Animal.alive.append(self)
         self.hidden = False
+
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, value):
+        self._health = value
+        if self._health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
     def __repr__(self) -> str:
         represent = (
@@ -27,7 +40,5 @@ class Carnivore(Animal):
 
     def bite(self, prey: Animal) -> None:
         if isinstance(prey, Herbivore) and not prey.hidden:
-            prey.health -= 50
+            prey.health = prey.health - 50
             print(f"{prey.name} is bitten")
-            if prey.health == 0:
-                Animal.alive.remove(prey)
